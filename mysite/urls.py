3b807b25e85f9a7d426c0f11a84a2a57e08ed2f9
ryo <r.hira.22.9.629@4nonome.com>
2023-03-07 16:47:21 +0900

    create static folder

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -30,3 +31,5 @@ if settings.SQL_DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
